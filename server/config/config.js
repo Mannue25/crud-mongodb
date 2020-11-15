8da5ddda4c28d4d7aa330691ca3e9d955384eab9
mannue25 <manuelgaleana2590@gmail.com>
2020-11-15 00:56:06 -0600

    Variable de enterno

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){ 
    urlDB="mongodb://localhost:27017/cafe"
 } else {
-    urlDB="mongodb+srv://Manuel_G04:GaleanaManuel04@cafe.qgl7j.mongodb.net/cafe"
+    urlDB=MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
